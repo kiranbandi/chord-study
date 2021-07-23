17d9542c0bc2f7c0c3510fdae3a02bca318afea9
kiranbandi <bvenkatkiran@gmail.com>
2021-07-23 15:49:42 -0600

    reset model to old one

--- app/my_blueprint/models.py
+++ app/my_blueprint/models.py
@@ -1,6 +1,6 @@
 def create(db):
     class chord(db.Model):
-        __tablename__ = "chordV04"
+        __tablename__ = "chordV03"
         ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
         participantID = db.Column(db.Integer, db.ForeignKey('participant.participantID'))
         answeredOn = db.Column(db.DateTime, nullable=False, default=db.func.now())
